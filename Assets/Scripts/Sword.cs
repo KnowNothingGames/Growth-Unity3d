1be761cd38414bf438d50d8e6b834358fe556943
Drakaven <KnowNothingGames@gmail.com>
2015-01-17 03:47:00 -0500

    Base Load
    Unity3d game based on
    2d platform tutorial
    
    added
    multi-jump
    instantiated weapons
    animations

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sword : MonoBehaviour {
+
+
+    private PlayerControl playerCtrl;		// Reference to the PlayerControl script.
+    private Animator anim;					// Reference to the Animator component.
+    
+    
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
